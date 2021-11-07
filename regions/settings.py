bed1ff1dbd0cdef268b6cb9899d0456bc30361a7
VovaSavin <volodimirsavin56@gmail.com>
2021-11-07 12:45:45 +0200

    Изменение настроек сервера

--- regions/settings.py
+++ regions/settings.py
@@ -142,3 +142,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
